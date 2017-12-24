26b47c538a542a8cd0a9fd91aef6dd844a3002a9
kasra-nejad <pfdclover@gmail.com>
2017-12-23 22:25:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-function initMap(x,y) {
+
+function initMap() {
   let lati = document.querySelector('.lat');
   let longi = document.querySelector('.lon');
+  let zoom = document.querySelector('.zoom');
   let button = document.querySelector('.button');
+  let inputs = document.querySelectorAll('input');
   let latVal = 0;
   let lonVal = 0;
+  let zoomVal = 4;
 
-  function updateLocation(){
-    latVal = Number(lati.value);
-    lonVal = Number(longi.value);
-    initMap(latVal,lonVal);
-  }
+   function enter(){
+     event.preventDefault();
+        if (event.keyCode === 13) {
+        initMap();
+   }
+ }
 
-  button.addEventListener('click',updateLocation);
+  button.addEventListener('click',initMap);
+  inputs.forEach(item => item.addEventListener("keyup", enter));
+
   var uluru = {
-    lat: x,
-    lng: y
+    lat: Number(lati.value),
+    lng: Number(longi.value)
   };
+
   var map = new google.maps.Map(document.querySelector('.map'), {
-    zoom: 4,
+    zoom: Number(zoom.value),
     center: uluru
   });
   var marker = new google.maps.Marker({
